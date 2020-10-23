2eaec6cce1c7f1402e467b0161077dcad1ee315e
Cervon Wong <tcrammy@gmail.com>
2020-10-23 22:06:28 +0800

    Feat: Add colour constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,65 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'dart:ui';
+
+// Primary Colors
+const Color kPrimaryColor = Color(0xff7b68ee);
+const Color kPrimaryLightColor = Color(0xffdfdbfb);
+
+// Secondary Colors
+const Color kSecondaryCyanColor = Color(0xff49ccf9);
+const Color kSecondaryPinkColor = Color(0xfffd71af);
+const Color kSecondaryYellowColor = Color(0xffffc800);
+const Color kSecondaryGreenColor = Color(0xff67cb48);
+
+// Accent Colors
+const Color kAccentRedColor = Color(0xfffc575e);
+const Color kAccentOrangeColor = Color(0xffff8600);
+const Color kAccentYellowColor = Color(0xfffcb410);
+const Color kAccentGreenColor = Color(0xff27ae60);
+const Color kAccentBlueColor = Color(0xff528ccb);
+const Color kAccentPurpleColor = Color(0xffbf4acc);
+const Color kAccentGrayColor = Color(0xff34495e);
+const Color kAccentBrownColor = Color(0xffb17e22);
+
+// Text Fill Colors
+const Color kTextFillRedColor = Color(0xffe03e3e);
+const Color kTextFillOrangeColor = Color(0xffd9730d);
+const Color kTextFillYellowColor = Color(0xffdfab01);
+const Color kTextFillBlueColor = Color(0xff0b6e99);
+const Color kTextFillPurpleColor = Color(0xff6940a5);
+const Color kTextFillMagentaColor = Color(0xffad1a72);
+const Color kTextFillBlackColor = Color(0xff343434);
+const Color kTextFillBrownColor = Color(0xff64473a);
+
+// Text Highlight Colors
+const Color kTextHighlightPinkColor = Color(0xffffe0e0);
+const Color kTextHighlightOrangeColor = Color(0xffffdbba);
+const Color kTextHighlightYellowColor = Color(0xfffffa78);
+const Color kTextHighlightGreenColor = Color(0xffe6ffcf);
+const Color kTextHighlightBlueColor = Color(0xffcae4f6);
+const Color kTextHighlightPurpleColor = Color(0xffeedbf6);
+
+// Neutrals
+const Color kNeutral100 = Color(0xfffafbfc);
+const Color kNeutral200 = Color(0xfff3f4f5);
+const Color kNeutral300 = Color(0xffb9bec7);
+const Color kNeutral500 = Color(0xff7c828d);
+const Color kNeutral900 = Color(0xff292d34);
