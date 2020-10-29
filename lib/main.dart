b23cddec09a66b5a7cb4d0eb4492843402f743db
Cervon Wong <tcrammy@gmail.com>
2020-10-29 11:54:32 +0800

    Feat: Add primary and accent textThemes, and ThemeData colors

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,8 @@ class SonaApp extends StatelessWidget {
 ThemeData _createThemeData() {
   return _createBaseThemeData().copyWith(
     colorScheme: _createColorScheme(),
+    primaryTextTheme: _createPrimaryAndAccentTextTheme(),
+    accentTextTheme: _createPrimaryAndAccentTextTheme(),
     textTheme: _createTextTheme(),
     bottomNavigationBarTheme: _createBottomNavigationBarTheme(),
   );
@@ -48,6 +50,16 @@ ThemeData _createThemeData() {
 
 ThemeData _createBaseThemeData() {
   return ThemeData(
+    // Primary
+    primaryColor: kPrimaryColor,
+    primaryColorDark: kPrimaryDarkColor,
+    primaryColorLight: kPrimaryLightColor,
+
+    // Secondary
+    accentColor: kSecondaryCyanColor,
+
+    backgroundColor: kNeutralColor2,
+    errorColor: kErrorColor,
     scaffoldBackgroundColor: kNeutralColor2,
   );
 }
@@ -82,66 +94,162 @@ ColorScheme _createColorScheme() {
 
 TextTheme _createTextTheme() {
   return GoogleFonts.workSansTextTheme(
-    const TextTheme(
+    TextTheme(
       headline1: TextStyle(
+        color: kNeutralColor8,
         fontSize: 102,
         fontWeight: FontWeight.w300,
         letterSpacing: -1.5,
       ),
       headline2: TextStyle(
+        color: kNeutralColor8,
         fontSize: 64,
         fontWeight: FontWeight.w300,
         letterSpacing: -0.5,
       ),
       headline3: TextStyle(
+        color: kNeutralColor8,
         fontSize: 51,
         fontWeight: FontWeight.w400,
       ),
       headline4: TextStyle(
+        color: kNeutralColor8,
         fontSize: 36,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.25,
       ),
       headline5: TextStyle(
+        color: kNeutralColor8,
         fontSize: 25,
         fontWeight: FontWeight.w400,
       ),
       headline6: TextStyle(
+        color: kNeutralColor8,
         fontSize: 21,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.15,
       ),
       subtitle1: TextStyle(
+        color: kNeutralColor8,
         fontSize: 17,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.15,
       ),
       subtitle2: TextStyle(
+        color: kNeutralColor8,
         fontSize: 15,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.1,
       ),
       bodyText1: TextStyle(
+        color: kNeutralColor8,
         fontSize: 17,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.5,
       ),
       bodyText2: TextStyle(
+        color: kNeutralColor8,
         fontSize: 15,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.25,
       ),
       button: TextStyle(
+        color: kNeutralColor8,
         fontSize: 15,
         fontWeight: FontWeight.w500,
         letterSpacing: 1.25,
       ),
       caption: TextStyle(
+        color: kNeutralColor8.withOpacity(0.6),
         fontSize: 13,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.4,
       ),
       overline: TextStyle(
+        color: kNeutralColor8,
+        fontSize: 11,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 1.5,
+      ),
+    ),
+  );
+}
+
+TextTheme _createPrimaryAndAccentTextTheme() {
+  return GoogleFonts.workSansTextTheme(
+    TextTheme(
+      headline1: TextStyle(
+        color: Colors.white,
+        fontSize: 102,
+        fontWeight: FontWeight.w300,
+        letterSpacing: -1.5,
+      ),
+      headline2: TextStyle(
+        color: Colors.white,
+        fontSize: 64,
+        fontWeight: FontWeight.w300,
+        letterSpacing: -0.5,
+      ),
+      headline3: TextStyle(
+        color: Colors.white,
+        fontSize: 51,
+        fontWeight: FontWeight.w400,
+      ),
+      headline4: TextStyle(
+        color: Colors.white,
+        fontSize: 36,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      headline5: TextStyle(
+        color: Colors.white,
+        fontSize: 25,
+        fontWeight: FontWeight.w400,
+      ),
+      headline6: TextStyle(
+        color: Colors.white,
+        fontSize: 21,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.15,
+      ),
+      subtitle1: TextStyle(
+        color: Colors.white,
+        fontSize: 17,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.15,
+      ),
+      subtitle2: TextStyle(
+        color: Colors.white,
+        fontSize: 15,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.1,
+      ),
+      bodyText1: TextStyle(
+        color: Colors.white,
+        fontSize: 17,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.5,
+      ),
+      bodyText2: TextStyle(
+        color: Colors.white,
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      button: TextStyle(
+        color: Colors.white,
+        fontSize: 15,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 1.25,
+      ),
+      caption: TextStyle(
+        color: Colors.white.withOpacity(0.6),
+        fontSize: 13,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.4,
+      ),
+      overline: TextStyle(
+        color: Colors.white,
         fontSize: 11,
         fontWeight: FontWeight.w400,
         letterSpacing: 1.5,
