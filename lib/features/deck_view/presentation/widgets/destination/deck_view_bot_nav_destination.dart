e0ec4a378756e7f61861e8528448483a8318a5d9
Cervon Wong <tcrammy@gmail.com>
2020-10-29 16:24:18 +0800

    Feat: Add ListView and custom AppBar with TextField

--- lib/features/deck_view/presentation/widgets/destination/deck_view_bot_nav_destination.dart
+++ lib/features/deck_view/presentation/widgets/destination/deck_view_bot_nav_destination.dart
@@ -19,11 +19,13 @@
 
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:sona_flutter/core/constants/app_colors.dart';
 import 'package:sona_flutter/core/presentation/bottom_navigation/bot_nav_destination.dart';
 
 class DeckViewBotNavDestination extends BotNavDestination {
   DeckViewBotNavDestination()
       : super(
+          appBar: _TextFieldAppBar(),
           label: 'Decks',
           icon: Icon(FluentIcons.dictionary_24_regular),
           activeIcon: Icon(FluentIcons.dictionary_24_filled),
@@ -31,8 +33,73 @@ class DeckViewBotNavDestination extends BotNavDestination {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Placeholder(),
+    return ListView(
+      children: [
+        for (var i = 0; i < 50; i++)
+          Ink(
+            color: Theme.of(context).colorScheme.surface,
+            child: ListTile(
+              title: Text(
+                'Deck Title',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              subtitle: Text(
+                '15 Due · 1000 Total · 90% Seen',
+                style: Theme.of(context).textTheme.caption,
+              ),
+              leading: AspectRatio(
+                aspectRatio: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    color:
+                        i % 2 == 0 ? kSecondaryCyanColor : kSecondaryPinkColor,
+                  ),
+                ),
+              ),
+              onTap: () => print('Tapped'),
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+class _TextFieldAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      title: _AppBarTextField(),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class _AppBarTextField extends StatelessWidget {
+  final _textFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _textFieldController,
+      decoration: InputDecoration(
+        hintText: 'Search Decks',
+        border: InputBorder.none,
+        icon: Icon(FluentIcons.book_formula_lookup_24_regular),
+        suffixIcon: Container(
+          child: IconButton(
+            icon: Icon(FluentIcons.dismiss_24_regular),
+            onPressed: () => _textFieldController.clear(),
+          ),
+        ),
+        suffixIconConstraints: BoxConstraints(
+          maxHeight: 40.0,
+          maxWidth: 40.0,
+        ),
+      ),
     );
   }
 }
