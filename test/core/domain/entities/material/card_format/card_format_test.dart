d7fbf045ec8158c758c4a85a3e7137a3f042d9c4
Cervon Wong <tcrammy@gmail.com>
2020-11-01 11:56:26 +0800

    Feat: Add card_format.dart
    Also add tests for CardFormat.

---
+++ test/core/domain/entities/material/card_format/card_format_test.dart
@@ -0,0 +1,124 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:sona_flutter/core/domain/entities/material/card_format/card_format.dart';
+import 'package:sona_flutter/core/domain/entities/material/card_format/card_format_structure.dart';
+
+// ignore: must_be_immutable
+class MockCardFormatStructure extends Mock implements CardFormatStructure {}
+
+void main() {
+  group(
+    'CardFormat when constructed',
+    () {
+      test(
+        'without optional parameters, '
+        'should have expected default fields',
+        () {
+          final cardFormat = CardFormat(name: 'Basic Front Format');
+
+          expect(cardFormat.name, 'Basic Front Format');
+          expect(cardFormat.front, CardFormatStructure.empty());
+          expect(cardFormat.back, CardFormatStructure.empty());
+        },
+      );
+
+      test(
+        'with null parameters, '
+        'should fail asserts',
+        () {
+          expect(
+            () => CardFormat(name: null),
+            throwsAssertionError,
+          );
+
+          expect(
+            () => CardFormat(name: 'Null name', front: null),
+            throwsAssertionError,
+          );
+
+          expect(
+            () => CardFormat(name: 'Null name', back: null),
+            throwsAssertionError,
+          );
+        },
+      );
+
+      test(
+        'with illegal name, '
+        'should fail asserts',
+        () {
+          expect(
+            () => CardFormat(name: 'x' * 151),
+            // name > 150 chars is illegal.
+            throwsAssertionError,
+          );
+
+          expect(
+            () => CardFormat(name: 'x' * 150),
+            // name <= 150 chars is legal.
+            isNot(throwsAssertionError),
+          );
+        },
+      );
+    },
+  );
+
+  test(
+    'CardFormat copyWith, '
+    'should return CardFormat with expected altered fields',
+    () {
+      final structure1 = MockCardFormatStructure();
+      final structure2 = MockCardFormatStructure();
+
+      final cardFormat1 = CardFormat(
+        name: 'First name',
+        front: structure1,
+        back: structure2,
+      );
+      expect(cardFormat1.name, 'First name');
+      expect(cardFormat1.front, structure1);
+      expect(cardFormat1.back, structure2);
+
+      final cardFormat2 = cardFormat1.copyWith(
+        name: 'New name',
+        front: structure2,
+        back: structure1,
+      );
+      expect(cardFormat2.name, 'New name');
+      expect(cardFormat2.front, structure2);
+      expect(cardFormat2.back, structure1);
+    },
+  );
+
+  test(
+    'CardFormat when equating logically equal CardFormats, '
+    'should return true',
+    () {
+      final cardFormat = CardFormat(
+        name: 'Equal name',
+        front: MockCardFormatStructure(),
+        back: MockCardFormatStructure(),
+      );
+      expect(cardFormat, cardFormat.copyWith());
+    },
+  );
+}
