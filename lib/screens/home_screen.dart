973810900bf31eb48d4010422bee5db45a090b0c
Cervon Wong <tcrammy@gmail.com>
2020-11-07 14:19:40 +0800

    Feat: Update elevation on AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,6 +28,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        elevation: 0.0,
         title: Text('Home'),
       ),
       drawer: CustomDrawer(),
