8ea19af81465693e250c7ea0267726fed88de3ea
Cervon Wong <tcrammy@gmail.com>
2020-12-15 13:07:01 +0800

    Feat: Add length constraints for string fields of Deck

---
+++ lib/core/constants/material_constants.dart
@@ -0,0 +1,27 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Constraints
+// Currently, constraints are only set for Deck as a test.
+const int kMinDeckNameLength = 5;
+const int kMaxDeckNameLength = 150;
+const int kMinDeckAuthorNameLength = 1;
+const int kMaxDeckAuthorNameLength = 50;
+const int kMinDeckDescriptionLength = 10;
+const int kMaxDeckDescriptionLenth = 255;
