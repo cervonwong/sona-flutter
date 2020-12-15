4e9318249e1e5100a3ee72b279db17c84fc35905
Cervon Wong <tcrammy@gmail.com>
2020-12-15 10:40:19 +0800

    Feat: Implement initial moor database

---
+++ lib/core/data/data_sources/moor/moor_database.dart
@@ -0,0 +1,45 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'dart:io';
+
+import 'package:moor/ffi.dart';
+import 'package:moor/moor.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+part 'moor_database.g.dart';
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await path_provider.getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, 'db.sqlite'));
+    return VmDatabase(file, logStatements: true);
+  });
+}
+
+@UseMoor()
+class MoorDatabase extends _$MoorDatabase {
+  MoorDatabase() : super(_openConnection());
+
+  // Update this each time the database structure (tables) have been changed
+  // after this app has been released.
+  @override
+  int get schemaVersion => 1;
+}
