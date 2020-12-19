b3e2c3b1e381929590d6aad1b9da2d553c7840c1
Cervon Wong <tcrammy@gmail.com>
2020-12-19 17:46:03 +0800

    Refactor(ui): Format color hex values to use CAPITAL LETTERS

--- lib/core/presentation/constants/color_constants.dart
+++ lib/core/presentation/constants/color_constants.dart
@@ -21,32 +21,32 @@ import 'dart:ui';
 
 // Primary Colors
 // The main color in the UI.
-const Color kPrimaryColor = Color(0xff7b68ee);
-const Color kPrimaryLightColor = Color(0xffdfdbfb);
-const Color kPrimaryDarkColor = Color(0xff5c4dbc);
+const Color kPrimaryColor = Color(0xFF7B68EE);
+const Color kPrimaryLightColor = Color(0xFFDFDBFB);
+const Color kPrimaryDarkColor = Color(0xFF5C4DBC);
 
 // Secondary Colors (Always use this order in lists)
 // Use the first three colors (Pink, Cyan, Yellow) mainly.
 // Use the rest of the colors when many colors are needed (e.g. stats charts).
-const Color kSecondaryPinkColor = Color(0xfffd71af);
-const Color kSecondaryCyanColor = Color(0xff49ccf9);
-const Color kSecondaryYellowColor = Color(0xffffc800);
-const Color kSecondaryGreenColor = Color(0xff00b884);
-const Color kSecondaryRedColor = Color(0xfffd7171);
-const Color kSecondaryTurquoiseColor = Color(0xff44ddcc);
-const Color kSecondaryBlueColor = Color(0xff5577ff);
-const Color kSecondaryPurpleColor = Color(0xffb660dd);
+const Color kSecondaryPinkColor = Color(0xFFFD71AF);
+const Color kSecondaryCyanColor = Color(0xFF49CCF9);
+const Color kSecondaryYellowColor = Color(0xFFFFC800);
+const Color kSecondaryGreenColor = Color(0xFF00B884);
+const Color kSecondaryRedColor = Color(0xFFFD7171);
+const Color kSecondaryTurquoiseColor = Color(0xFF44DDCC);
+const Color kSecondaryBlueColor = Color(0xFF5577FF);
+const Color kSecondaryPurpleColor = Color(0xFFb660DD);
 
 // Accent Colors
 // Reserved for denotative colors.
-const Color _kAccentRedColor = Color(0xfffc575e);
-const Color _kAccentOrangeColor = Color(0xffff8600);
-const Color _kAccentYellowColor = Color(0xfffcb410);
-const Color _kAccentGreenColor = Color(0xff27ae60);
-const Color _kAccentBlueColor = Color(0xff528ccb);
-const Color _kAccentPurpleColor = Color(0xffbf4acc);
-const Color _kAccentGrayColor = Color(0xff34495e);
-const Color _kAccentBrownColor = Color(0xffb17e22);
+const Color _kAccentRedColor = Color(0xFFFC575E);
+const Color _kAccentOrangeColor = Color(0xFFFF8600);
+const Color _kAccentYellowColor = Color(0xFFFCB410);
+const Color _kAccentGreenColor = Color(0xFF27AE60);
+const Color _kAccentBlueColor = Color(0xFF528CCB);
+const Color _kAccentPurpleColor = Color(0xFFBF4ACC);
+const Color _kAccentGrayColor = Color(0xFF34495E);
+const Color _kAccentBrownColor = Color(0xFFB17E22);
 
 // Denotative Colors
 // Specify a certain state in the UI.
@@ -57,24 +57,24 @@ const Color kSuccessColor = _kAccentGreenColor;
 // Text Fill Colors
 // User is able to select from these colors when choosing a text color
 // when creating the style of a card format.
-const Color kTextFillRedColor = Color(0xffe03e3e);
-const Color kTextFillOrangeColor = Color(0xffd9730d);
-const Color kTextFillYellowColor = Color(0xffdfab01);
-const Color kTextFillBlueColor = Color(0xff0b6e99);
-const Color kTextFillPurpleColor = Color(0xff6940a5);
-const Color kTextFillMagentaColor = Color(0xffad1a72);
-const Color kTextFillBlackColor = Color(0xff343434);
-const Color kTextFillBrownColor = Color(0xff64473a);
+const Color kTextFillRedColor = Color(0xFFE03E3E);
+const Color kTextFillOrangeColor = Color(0xFFD9730D);
+const Color kTextFillYellowColor = Color(0xFFDFAB01);
+const Color kTextFillBlueColor = Color(0xFF0B6E99);
+const Color kTextFillPurpleColor = Color(0xFF6940A5);
+const Color kTextFillMagentaColor = Color(0xFFAD1A72);
+const Color kTextFillBlackColor = Color(0xFF343434);
+const Color kTextFillBrownColor = Color(0xFF64473A);
 
 // Text Highlight Colors
 // User is able to select from these colors when choosing a text highlight color
 // when creating the style of a card format.
-const Color kTextHighlightPinkColor = Color(0xffffe0e0);
-const Color kTextHighlightOrangeColor = Color(0xffffdbba);
-const Color kTextHighlightYellowColor = Color(0xfffffa78);
-const Color kTextHighlightGreenColor = Color(0xffe6ffcf);
-const Color kTextHighlightBlueColor = Color(0xffcae4f6);
-const Color kTextHighlightPurpleColor = Color(0xffeedbf6);
+const Color kTextHighlightPinkColor = Color(0xFFFFE0E0);
+const Color kTextHighlightOrangeColor = Color(0xFFFFDBBA);
+const Color kTextHighlightYellowColor = Color(0xFFFFFA78);
+const Color kTextHighlightGreenColor = Color(0xFFE6FFCF);
+const Color kTextHighlightBlueColor = Color(0xFFCAE4F6);
+const Color kTextHighlightPurpleColor = Color(0xFFEEDBF6);
 
 // Neutrals
 // Used in texts, dividers, backgrounds, surfaces, and other UI components.
@@ -93,19 +93,19 @@ const Color kNeutralColor1 = Color(0xFFFCFCFC);
 
 // Dark text colors use kNeutralColor8 as a base.
 const double kDarkHighEmphasisOpacity = 0.90;
-const Color kDarkHighEmphasisTextColor = Color(0xe614142B); // E6 --> 90%.
+const Color kDarkHighEmphasisTextColor = Color(0xE614142B); // E6 --> 90%.
 const double kDarkMediumEmphasisOpacity = 0.65;
-const Color kDarkMediumEmphasisTextColor = Color(0xa614142B); // A6 --> 65%.
+const Color kDarkMediumEmphasisTextColor = Color(0xA614142B); // A6 --> 65%.
 const double kDarkLowEmphasisOpacity = 0.30;
-const Color kDarkLowEmphasisTextColor = Color(0x4d14142B); // 4D --> 30%.
+const Color kDarkLowEmphasisTextColor = Color(0x4D14142B); // 4D --> 30%.
 
 // Light text colors use pure white (#FFFFFF) as a base.
 const double kLightHighEmphasisOpacity = 1.00;
-const Color kLightHighEmphasisTextColor = Color(0xffffffff); // FF --> 100%.
+const Color kLightHighEmphasisTextColor = Color(0xFFFFFFFF); // FF --> 100%.
 const double kLightMediumEmphasisOpacity = 0.70;
-const Color kLightMediumEmphasisTextColor = Color(0xb3ffffff); // B3 --> 70%.
+const Color kLightMediumEmphasisTextColor = Color(0xB3FFFFFF); // B3 --> 70%.
 const double kLightLowEmphasisOpacity = 0.35;
-const Color kLightLowEmphasisTextColor = Color(0x59ffffff); // 59 --> 35%.
+const Color kLightLowEmphasisTextColor = Color(0x59FFFFFF); // 59 --> 35%.
 
 // Splashes and Highlights
 // Overlaid on interactive components with a ripple effect (Ink).
@@ -119,6 +119,6 @@ const Color kLightSplashColor =
 const Color kLightHighlightColor =
     Color(0x3DFCFCFC); // kNeutralColor1.withOpacity(0.24);
 const Color kPrimarySplashColor =
-    Color(0x1f7b68ee); // kPrimaryColor.withOpacity(0.12);
+    Color(0x1F7B68EE); // kPrimaryColor.withOpacity(0.12);
 const Color kPrimaryHighlightColor =
-    Color(0x1f7b68ee); // kPrimaryColor.withOpacity(0.12);
+    Color(0x1F7B68EE); // kPrimaryColor.withOpacity(0.12);
