65296ed57e12f7ed565b32c52b6259464dcb34a9
Cervon Wong <tcrammy@gmail.com>
2020-12-19 22:24:48 +0800

    Feat(moor): Create initial TagsDao

---
+++ lib/core/data/data_sources/moor/tags/tags_dao.dart
@@ -0,0 +1,30 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'package:moor/moor.dart';
+
+import '../moor_database.dart';
+import 'tags_table.dart';
+
+part 'tags_dao.g.dart';
+
+@UseDao(tables: [Tags])
+class TagsDao extends DatabaseAccessor<MoorDatabase> with _$TagsDaoMixin {
+  TagsDao(MoorDatabase db) : super(db);
+}
---
+++ lib/core/data/data_sources/moor/tags/tags_dao.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tags_dao.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$TagsDaoMixin on DatabaseAccessor<MoorDatabase> {
+  $TagsTable get tags => attachedDatabase.tags;
+}
