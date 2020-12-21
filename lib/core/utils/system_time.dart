c2f153c8cbc558028b3fb29ee321da495b775c34
Cervon Wong <tcrammy@gmail.com>
2020-12-21 13:35:36 +0800

    Feat(core): Create class to wrap DateTime.now() for testing

---
+++ lib/core/utils/system_time.dart
@@ -0,0 +1,28 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// This is responsible for getting the current DateTime.
+///
+/// This allows for dependency injection of this into classes which need the
+/// current DateTime. Unit testing will now be possible.
+///
+/// See also: https://ayende.com/blog/3408/dealing-with-time-in-tests.
+class SystemTime {
+  DateTime now() => DateTime.now();
+}
