dac6c2c2b0ead1122e75af491538a898b651af67
Cervon Wong <tcrammy@gmail.com>
2020-12-22 13:03:05 +0800

    Feat(pres): Initialize BLoC for decks

---
+++ lib/features/deck/presentation/bloc/deck_event.dart
@@ -0,0 +1,63 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+part of 'deck_bloc.dart';
+
+abstract class DeckEvent extends Equatable {
+  const DeckEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+/// Tells the BloC that it needs to load the list of decks.
+///
+/// This is immediately added to the BLoC when created to request the latest
+/// decks.
+class DeckInitialized extends DeckEvent {}
+
+/// Tells the BloC that it needs to add a new deck to the list of decks.
+class DeckAdded extends DeckEvent {
+  final Deck deck;
+
+  const DeckAdded({@required this.deck});
+
+  @override
+  List<Object> get props => [deck];
+}
+
+/// Tells the BloC that it needs to update an existing deck.
+class DeckUpdated extends DeckEvent {
+  final Deck deck;
+
+  const DeckUpdated({@required this.deck});
+
+  @override
+  List<Object> get props => [deck];
+}
+
+/// Tells the BloC that it needs to delete an existing deck.
+class DeckDeleted extends DeckEvent {
+  final Deck deck;
+
+  const DeckDeleted({@required this.deck});
+
+  @override
+  List<Object> get props => [deck];
+}
---
+++ lib/features/deck/presentation/bloc/deck_state.dart
@@ -0,0 +1,46 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+part of 'deck_bloc.dart';
+
+abstract class DeckState extends Equatable {
+  const DeckState();
+
+  @override
+  List<Object> get props => [];
+}
+
+/// The default state.
+class DeckInitial extends DeckState {}
+
+/// The state while the BLoC is fetching the list of decks from the repository.
+class DeckLoading extends DeckState {}
+
+/// The state when the BLoC has successfully loaded the list of decks.
+class DeckLoaded extends DeckState {
+  final List<Deck> decks;
+
+  const DeckLoaded({@required this.decks});
+
+  @override
+  List<Object> get props => [decks];
+}
+
+/// The state when the BLoC has not successfully loaded the list of decks.
+class DeckLoadFailed extends DeckState {}
