52d6084f2e7eaaa6c6ee71a5706b6fc412d33e99
Cervon Wong <tcrammy@gmail.com>
2020-12-23 12:26:45 +0800

    Feat(di): Try to implement GetIt service locator

---
+++ lib/features/deck/deck_injection_container.dart
@@ -0,0 +1,53 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'package:get_it/get_it.dart';
+import '../../core/data/data_sources/moor/decks/decks_dao.dart';
+import '../../core/data/data_sources/moor/moor_database.dart';
+import '../../core/data/repositories/material/deck/deck_repository_impl.dart';
+
+import 'domain/use_cases/create_deck.dart';
+import 'domain/use_cases/delete_deck.dart';
+import 'domain/use_cases/get_all_decks.dart';
+import 'presentation/bloc/deck_bloc.dart';
+
+void initDeck(GetIt getIt) {
+  // Presentation
+  getIt.registerFactory(() {
+    return DeckBloc(
+      createDeck: getIt(),
+      getAllDecks: getIt(),
+      deleteDeck: getIt(),
+    );
+  });
+
+  // Domain
+  getIt.registerLazySingleton(() => CreateDeck(repository: getIt()));
+  getIt.registerLazySingleton(() => GetAllDecks(repository: getIt()));
+  getIt.registerLazySingleton(() => DeleteDeck(repository: getIt()));
+
+  // Data
+  getIt.registerLazySingleton(() => DeckRepositoryImpl(decksDao: getIt()));
+
+  getIt.registerLazySingleton(() {
+    return DecksDaoImpl(db: getIt(), systemTime: getIt());
+  });
+
+  getIt.registerSingleton(() => MoorDatabase.mobile());
+}
