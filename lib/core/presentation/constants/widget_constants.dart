11f5282e733807939f1663d0efd3c6a1e0956976
Cervon Wong <tcrammy@gmail.com>
2021-02-18 19:18:13 +0800

    Feat(ui): Add padding constants

--- lib/core/presentation/constants/widget_constants.dart
+++ lib/core/presentation/constants/widget_constants.dart
@@ -21,3 +21,27 @@
 const double kSmallCornerRadius = 12.0;
 const double kMediumCornerRadius = 16.0;
 const double kLargeCornerRadius = 32.0;
+
+// Spacing Scale
+// Padding within the context of a component, for more refined spacing.
+// See: https://www.carbondesignsystem.com/guidelines/spacing/overview/#spacing-scale.
+const double kSpacingPadding1 = 2.0;
+const double kSpacingPadding2 = 4.0;
+const double kSpacingPadding3 = 8.0;
+const double kSpacingPadding4 = 12.0;
+const double kSpacingPadding5 = 16.0;
+const double kSpacingPadding6 = 24.0;
+const double kSpacingPadding7 = 32.0;
+const double kSpacingPadding8 = 40.0;
+const double kSpacingPadding9 = 48.0;
+
+// Layout Scale
+// Padding between components and other UI parts into a screen.
+// See: https://www.carbondesignsystem.com/guidelines/spacing/overview/#spacing-scale.
+const double kLayoutPadding1 = 16.0;
+const double kLayoutPadding2 = 24.0;
+const double kLayoutPadding3 = 32.0;
+const double kLayoutPadding4 = 48.0;
+const double kLayoutPadding5 = 64.0;
+const double kLayoutPadding6 = 96.0;
+const double kLayoutPadding7 = 160.0;
