d8091559ddf40034e3dd4689fb62b983211c4c24
Cervon Wong <tcrammy@gmail.com>
2021-05-22 13:53:30 +0800

    Feat(ui): Implement rudimentary bottom nav with changing AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,27 +17,128 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+
 import '../features/stats/streak/presentation/widgets/streak_counter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  static const _itemLabels = ['Decks', 'Search', 'Progress', 'Profile'];
+  static const _unselectedIconData = [
+    FluentIcons.style_guide_24_regular,
+    FluentIcons.search_24_regular,
+    FluentIcons.arrow_growth_24_regular,
+    FluentIcons.person_24_regular,
+  ];
+  static const _selectedIconData = [
+    FluentIcons.style_guide_24_filled,
+    FluentIcons.search_24_filled,
+    FluentIcons.arrow_growth_24_filled,
+    FluentIcons.person_24_filled,
+  ];
+  static const _appBars = [
+    DecksDestinationAppBar(),
+    SearchDestinationAppBar(),
+    ProgressDestinationAppBar(),
+    ProfileDestinationAppBar(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Your Decks',
-        ),
-        actions: [
-          StreakCounter(
-            streakCount: 0,
-            isActive: false,
-          ),
+      appBar: _appBars[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          for (int i = 0; i < 4; i++)
+            BottomNavigationBarItem(
+              icon: Icon(
+                _currentIndex == i
+                    ? _selectedIconData[i]
+                    : _unselectedIconData[i],
+              ),
+              label: _itemLabels[i],
+            ),
         ],
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
       body: SafeArea(
         child: Container(),
       ),
     );
   }
+}
+
+class DecksDestinationAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const DecksDestinationAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Your Decks'),
+      actions: [
+        const StreakCounter(streakCount: 0, isActive: false),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class SearchDestinationAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const SearchDestinationAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Search'),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class ProgressDestinationAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const ProgressDestinationAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Progress'),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class ProfileDestinationAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const ProfileDestinationAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Profile'),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
