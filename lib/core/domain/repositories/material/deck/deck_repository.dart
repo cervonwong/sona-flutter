0d5f16ae5f3d91d5ec0edf1ed78fbb7453b8bcef
Cervon Wong <tcrammy@gmail.com>
2021-06-02 20:38:28 +0800

    Refactor(repository): Migrate deck_repository.dart (interface) to null safety

--- lib/core/domain/repositories/material/deck/deck_repository.dart
+++ lib/core/domain/repositories/material/deck/deck_repository.dart
@@ -1,5 +1,3 @@
-// @dart=2.9
-
 /*
  * Sona is a cross-platform educational app which helps you remember
  * facts easier, developed with Flutter.
@@ -19,29 +17,28 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import 'package:meta/meta.dart';
-
 import '../../../entities/material/card/card.dart';
 import '../../../entities/material/deck/deck.dart';
 import '../../../entities/material/entry/entry.dart';
 
 abstract class DeckRepository {
-  Future<Deck> create({@required String name});
+  Future<Deck> create({required String name});
 
-  Future<Deck> getById({@required int id});
+  Future<Deck?> getById({required int id});
 
-  Future<Deck> getByName({@required String name});
+  Future<Deck?> getByName({required String name});
 
   Future<List<Deck>> getAll();
 
-  Future<Deck> getByEntry({@required Entry entry});
+  Future<Deck> getByEntry({required Entry entry});
 
-  Future<Deck> getByCard({@required Card card});
+  Future<Deck> getByCard({required Card card});
 
   // TODO(cervonwong): 29/12/2020 Possibly change the return type to return Deck
   //  with the updated lastEditedDateTime? But this might not be necessary after
   //  using watches instead of getAlls... Let's wait and see.
-  Future<void> update({@required Deck deck});
+  // TODO: 6/2/2021 I think we need to rework this to return Deck as per DAO.
+  Future<void> update({required Deck deck});
 
-  Future<void> delete({@required Deck deck});
+  Future<void> delete({required Deck deck});
 }
