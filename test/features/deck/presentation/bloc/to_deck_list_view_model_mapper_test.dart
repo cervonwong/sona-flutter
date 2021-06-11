b5c193eaae257b44265a6d58c96a6868d50a74f0
Cervon Wong <tcrammy@gmail.com>
2021-06-11 22:09:39 +0800

    Test(bloc): Create test file for `ToDeckListViewModelMapper`

---
+++ test/features/deck/presentation/bloc/to_deck_list_view_model_mapper_test.dart
@@ -0,0 +1,24 @@
+/*
+ * Sona is a cross-platform educational app which helps you remember
+ * facts easier, developed with Flutter.
+ * Copyright (C) 2020, 2021 Cervon Wong
+ *
+ * Sona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+void main() {
+  // TODO: 6/11/2021 No tests can be written yet because the final
+  //  implementation has not been worked out. If I were doing TDD maybe I would
+  //  write it earlier? Anyway it seems to early.
+}
